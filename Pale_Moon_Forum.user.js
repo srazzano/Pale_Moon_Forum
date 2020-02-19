b02f814937b647f3b9f6cb6b050c7dc889d404dd
Sonny Razzano <srazzano@gmail.com>
2020-02-19 10:53:17 -0800

    Update Pale_Moon_Forum.user.js

--- Pale_Moon_Forum.user.js
+++ Pale_Moon_Forum.user.js
@@ -364,7 +364,8 @@
       .site_logo {background: url(https://raw.githubusercontent.com/srazzano/Images/master/logo.png) !important; height: 70px !important; width: 70px !important;}\
       .headerbar h1 {margin: 6px 0 0 0 !important;}\
       #aSep {margin: 0 6px !important;}\
-      #clearBtn {-moz-appearance: none !important; background: url(https://raw.githubusercontent.com/srazzano/Images/master/broom16.png) no-repeat center, linear-gradient(#F0F0F0, #E0E0E0) !important; border-right: 1px solid #C7C3BF !important; border-radius: 4px 0 0 4px !important; float: left !important; height: 24px !important; width: 29px !important;}\
+      #clearBtn {-moz-appearance: none !important; background: url(https://raw.githubusercontent.com/srazzano/Images/master/broom16.png) no-repeat center, linear-gradient(#F0F0F0, #E0E0E0) !important; border-right: 1px solid #C7C3BF !important; border-radius: 4px 0 0 4px !important; filter: grayscale(1) !important; float: left !important; height: 24px !important; width: 29px !important;}\
+      #clearBtn:hover {background: url(https://raw.githubusercontent.com/srazzano/Images/master/broom16.png) no-repeat center, linear-gradient(#E0E0E0, #F0F0F0) !important; filter: none !important;}\
       .search-header {margin-top: 30px !important;}\
       #search-box {float: right !important; width: 240px !important;}\
       .search-box button.search-icon, .search-box a.button {padding: 3px 4px 1px 2px !important;}\
